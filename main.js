0b64245385fae8715d107305ddbaca3e507f9906
Amir Mechkour <amirmechkour5474@gmail.com>
2025-10-18 09:46:19 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,38 +1,31 @@
-import { app, BrowserWindow } from 'electron'
 import path from 'path'
 import { fileURLToPath } from 'url'
-
+import { app, BrowserWindow } from 'electron'
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
 let mainWindow
-
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1400,
         height: 900,
-        title: StacX,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         },
         title: 'StacX',
     })
-
-    // load frontend
     mainWindow.loadFile(path.join(__dirname, 'public', 'index.html'))
-
-    // optional: open dev tools
-    // mainWindow.webContents.openDevTools()
 }
-
 app.whenReady().then(() => {
     createWindow()
-
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow()
     })
 })
-
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
+})
+require('electron-reload')(__dirname, {
+    electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
 })
